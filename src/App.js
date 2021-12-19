0bcd7d43c83a43b451ebc385773c8ce9201255ed
Levi Bravenboer <levibravenboer@gmail.com>
2021-12-19 16:11:18 +0100

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Header from "./Components/Header";
-import { OuterLayout } from "./Styles/Layouts";
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DiscoverPage from "./Pages/LandingsPages/DiscoverPage";
+import HowPage from "./Pages/LandingsPages/Howpage";
+import PricingPage from "./Pages/LandingsPages/PricingPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-      <OuterLayout></OuterLayout>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<DiscoverPage />}></Route>
+        <Route path="/how" element={<HowPage />}></Route>
+        <Route path="/pricing" element={<PricingPage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
