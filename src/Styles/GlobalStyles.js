e6844166e0f35c7125dba437397b64d5924549bd
Levi Bravenboer <levibravenboer@gmail.com>
2021-12-29 17:46:21 +0100

    move file to different folder

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -9,12 +9,13 @@ const GlobalStyle = createGlobalStyle`
     --semi-brown: rgba(86, 66, 62, 0.98);
     --semi-brown-hover: rgba(86, 66, 62, 0.8);
     --border-colour: #CAB6F1;
-    --cool-blue: #A6D0E7;
     --cool-blue-hover: #88baff;
 
     --outerlayout-bg-color: hsl(54, 63%, 91%);
     --innerlayout-bg-color: #FFFEF6;
     --green-color: hsla(158, 99%, 43%, 1);
+    --cool-blue: #D6FFFC;
+
 
 
     }
