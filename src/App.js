e7b30c14696c8a186aaf5b998a48dcdcb370783b
Levi Bravenboer <levibravenboer@gmail.com>
2021-12-29 17:45:59 +0100

    change routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DiscoverPage from "./Pages/LandingsPages/DiscoverPage";
 import HowPage from "./Pages/LandingsPages/Howpage";
 import PricingPage from "./Pages/LandingsPages/PricingPage";
+import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<DiscoverPage />}></Route>
-        <Route path="/how" element={<HowPage />}></Route>
         <Route path="/pricing" element={<PricingPage />}></Route>
+        <Route path="/how" element={<HowPage />}></Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
