2ca2e3a8ef129347e1472f367e78b42678192de8
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-09 20:44:04 +0100

    add popupmodal

---
+++ src/Components/AuthPopup/AuthPopupModal.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+function AuthPopupModal(props) {
+  return (
+    <StyledModalOverlay>
+      <StyledModal>
+        <StyledCloseButton to={"/"} onClick={props.closeFunction}>
+          &times;
+        </StyledCloseButton>
+        {props.type === "login" ? (
+          <LoginForm closeAuth={props.closeFunction}></LoginForm>
+        ) : null}
+        {props.type === "register" ? <RegisterForm></RegisterForm> : null}
+      </StyledModal>
+    </StyledModalOverlay>
+  );
+}
+const StyledModalOverlay = styled.div`
+  display: flex; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+`;
+const StyledModal = styled.div`
+  border-radius: 5px;
+  background-color: #fefefe;
+  margin: auto;
+  border: 1px solid #888;
+  width: 80%;
+  min-height: 40%;
+  position: relative;
+  transition: all 5s ease-in-out;
+`;
+
+const StyledCloseButton = styled(Link)`
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  transition: all 200ms;
+  font-size: 30px;
+  font-weight: bold;
+  text-decoration: none;
+  cursor: default;
+  &:hover {
+    color: var(--green-color);
+  }
+`;
+
+export default AuthPopupModal;
