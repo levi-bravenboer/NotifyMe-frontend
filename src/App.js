eaf5fc77ea8742133b8dfc967784f4c6a85e7c6f
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-09 20:37:20 +0100

    Change Router and routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DiscoverPage from "./Pages/LandingsPages/DiscoverPage";
 import HowPage from "./Pages/LandingsPages/Howpage";
 import PricingPage from "./Pages/LandingsPages/PricingPage";
 import ErrorPage from "./Pages/ErrorPage";
-
+import { AuthProvider } from "./Context/AuthContext";
+import AuthPopupModal from "./Components/AuthPopup/AuthPopupModal";
+import MainPage from "./Pages/AppPages/MainPage";
+import ProtectedRoute from "./Utils/ProtectedRoute";
 function App() {
+  const [showAuthModal, setShowAuthModal] = useState(false);
+  const [authModalType, setAuthModalType] = useState("");
+
+  const openAuthModal = (type) => {
+    setShowAuthModal(true);
+    setAuthModalType(type);
+  };
+  const closeAuthModal = () => {
+    console.log("close auth");
+    setShowAuthModal(false);
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<DiscoverPage />}></Route>
-        <Route path="/pricing" element={<PricingPage />}></Route>
-        <Route path="/how" element={<HowPage />}></Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={<DiscoverPage showModal={openAuthModal} />}
+          ></Route>
+          <Route
+            path="/pricing/"
+            element={<PricingPage showModal={openAuthModal} />}
+          ></Route>
+          <Route
+            path="/how/"
+            element={<HowPage showModal={openAuthModal} />}
+          ></Route>
+          <Route
+            path="/pricing/:type"
+            element={<PricingPage showModal={openAuthModal} />}
+          ></Route>
+          <Route
+            path="/how/:type"
+            element={<HowPage showModal={openAuthModal} />}
+          ></Route>
+          <Route
+            path=":type"
+            element={<DiscoverPage showModal={openAuthModal} />}
+          ></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/app" element={<MainPage />}></Route>
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        {showAuthModal ? (
+          <AuthPopupModal
+            type={authModalType}
+            closeFunction={closeAuthModal}
+          ></AuthPopupModal>
+        ) : null}
+      </AuthProvider>
     </Router>
   );
 }
