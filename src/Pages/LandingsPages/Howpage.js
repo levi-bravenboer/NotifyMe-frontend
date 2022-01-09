180b5cce8d8be7642ebf69975fb1e16f315618ac
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-09 20:45:51 +0100

    Add howpage

--- src/Pages/LandingsPages/Howpage.js
+++ src/Pages/LandingsPages/Howpage.js
@@ -1,22 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { OuterLayout, InnerLayout } from "../../Styles/Layouts";
 import Nav from "../../Components/Navbar/Nav";
+import { useParams } from "react-router-dom";
 
-class HowPage extends Component {
-  constructor(props) {
-    super(props);
-    console.log("");
-  }
-
-  render() {
-    return (
-      <OuterLayout>
-        <InnerLayout>
-          <Nav pricing={false} how={true} discover={false} />
-        </InnerLayout>
-      </OuterLayout>
-    );
-  }
+function HowPage(props) {
+  const params = useParams();
+  useEffect(() => {
+    if (
+      params.type === "login" ||
+      params.type === "register" ||
+      params.type === "reset" ||
+      params.type === "resetconfirm"
+    ) {
+      props.showModal(params.type);
+    }
+    return () => {};
+  }, [params.type]);
+  return (
+    <OuterLayout>
+      <InnerLayout>
+        <Nav showModal={props.showModal} type={params.type} how={true} />
+      </InnerLayout>
+    </OuterLayout>
+  );
 }
 
 export default HowPage;
