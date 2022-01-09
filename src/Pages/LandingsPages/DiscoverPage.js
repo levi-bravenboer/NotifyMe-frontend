d36dfe222e63d0e8c4f152b96d81028c5557cd03
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-09 20:45:45 +0100

    add DiscoverPage

--- src/Pages/LandingsPages/DiscoverPage.js
+++ src/Pages/LandingsPages/DiscoverPage.js
@@ -1,22 +1,29 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { OuterLayout, InnerLayout } from "../../Styles/Layouts";
 import Nav from "../../Components/Navbar/Nav";
+import { useParams } from "react-router-dom";
 
-class DiscoverPage extends Component {
-  constructor(props) {
-    super(props);
-    console.log("Test");
-  }
+function DiscoverPage(props) {
+  const params = useParams();
+  useEffect(() => {
+    if (
+      params.type === "login" ||
+      params.type === "register" ||
+      params.type === "reset" ||
+      params.type === "resetconfirm"
+    ) {
+      props.showModal(params.type);
+    }
+    return () => {};
+  }, [params.type]);
 
-  render() {
-    return (
-      <OuterLayout>
-        <InnerLayout>
-          <Nav pricing={false} how={false} discover={true} />
-        </InnerLayout>
-      </OuterLayout>
-    );
-  }
+  return (
+    <OuterLayout>
+      <InnerLayout>
+        <Nav showModal={props.showModal} type={params.type} discover={true} />
+      </InnerLayout>
+    </OuterLayout>
+  );
 }
 
 export default DiscoverPage;
