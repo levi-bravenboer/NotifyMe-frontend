68e51718936f6d975973ee7dada743be3a384bd0
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-09 20:46:10 +0100

    add pricing page

--- src/Pages/LandingsPages/PricingPage.js
+++ src/Pages/LandingsPages/PricingPage.js
@@ -1,22 +1,29 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { OuterLayout, InnerLayout } from "../../Styles/Layouts";
 import Nav from "../../Components/Navbar/Nav";
+import { useParams } from "react-router-dom";
 
-class PricingPage extends Component {
-  constructor(props) {
-    super(props);
-    console.log("");
-  }
+function PricingPage(props) {
+  const params = useParams();
+  useEffect(() => {
+    if (
+      params.type === "login" ||
+      params.type === "register" ||
+      params.type === "reset" ||
+      params.type === "resetconfirm"
+    ) {
+      props.showModal(params.type);
+    }
+    return () => {};
+  }, [params.type]);
 
-  render() {
-    return (
-      <OuterLayout>
-        <InnerLayout>
-          <Nav pricing={true} how={false} discover={false} />
-        </InnerLayout>
-      </OuterLayout>
-    );
-  }
+  return (
+    <OuterLayout>
+      <InnerLayout>
+        <Nav showModal={props.showModal} type={params.type} pricing={true} />
+      </InnerLayout>
+    </OuterLayout>
+  );
 }
 
 export default PricingPage;
