bf360fd5fd814c523bbf3da4e1fd5cf424044fe6
Levi Bravenboer <levibravenboer@gmail.com>
2022-01-10 18:53:28 +0100

    add comingsoon

---
+++ src/Components/AppComponents/ComingSoon.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+function ComingSoon() {
+  return (
+    <StyledContainer>
+      <StyledSubContainer>
+        <StyledFirstSection>
+          <ColumnTwo1>
+            <StyledContainerInline>
+              <h1>COMING SOON!</h1>
+            </StyledContainerInline>
+          </ColumnTwo1>
+        </StyledFirstSection>
+      </StyledSubContainer>
+    </StyledContainer>
+  );
+}
+
+const StyledContainerInline = styled.div`
+  height: 100%;
+  background-color: white;
+  margin: 0;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+  transition: 0.4s ease-in-out;
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+  }
+  h1 {
+    font-size: 4rem;
+  }
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: max-content;
+    width: 75%;
+    margin-top: 1rem;
+  }
+`;
+const StyledContainer = styled.div`
+  width: 80%;
+  background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%);
+  border-bottom-right-radius: 2rem;
+  border-top-right-radius: 2rem;
+  margin: 1rem 8rem 1rem 4rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 1rem 0 0 0;
+  }
+`;
+
+const StyledSubContainer = styled.div`
+  margin: 0.5rem 0;
+  height: 80%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: 100%;
+  }
+`;
+const StyledFirstSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 40%;
+  gap: 2rem;
+  width: 100%;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+    align-items: center;
+    height: max-content;
+  }
+`;
+
+const ColumnTwo1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 115%;
+  width: 100%;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: max-content;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export default ComingSoon;
