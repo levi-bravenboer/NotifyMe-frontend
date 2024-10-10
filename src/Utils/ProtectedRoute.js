2feb6fd98a22631070f5ecd7958cec0f6955bb07
Levi Bravenboer <levibravenboer@gmail.com>
2024-10-10 16:18:03 +0200

    temp commit for testing

--- src/Utils/ProtectedRoute.js
+++ src/Utils/ProtectedRoute.js
@@ -3,6 +3,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 import AuthContext from '../Context/AuthContext';
 
 const ProtectedRoute = () => {
+  console.log('___test');
   const authContext = useContext(AuthContext);
   return authContext.user ? <Outlet /> : <Navigate to="/login" />;
 };
